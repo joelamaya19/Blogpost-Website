726d5d8fe0a76aed1ef6cec1d29ec655c277cea7
unknown <joel.amaya99@gmail.com>
2024-04-01 00:29:31 -0500

    added blogs button to forwards to blogs and back button to return to main page

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,15 @@
-function toggleDarkTheme(event) {
-    console.log(event);
 
-    event.classList.toggle('enabled');
-    document.querySelectorAll('.post-blog, input[type="text"], textarea, header, footer, body').forEach(element => {
+function toggleDarkTheme(event) {
+    document.querySelectorAll('.post-blog, input[type="text"], textarea, header, footer, body, .blogs-web-button, .back-button, .dark-theme-toggle').forEach(element => {
         element.classList.toggle('dark');
+        element.classList.toggle('enabled');
     });
 
+}
+
+function navigateToBlogsPage() {
+    window.location.href = 'blogs.html';
+}
+function navigateBackToHome() {
+    window.location.href = 'index.html';
 }
