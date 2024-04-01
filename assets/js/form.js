26f8d6a89874a122f2d689589360777adc055877
unknown <joel.amaya99@gmail.com>
2024-04-01 13:31:44 -0500

    local storage to dark theme

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,10 +1,27 @@
 
-function toggleDarkTheme(event) {
-    document.querySelectorAll('.post-blog, input[type="text"], textarea, header, footer, body, .blogs-web-button, .back-button, .dark-theme-toggle').forEach(element => {
-        element.classList.toggle('dark');
-        element.classList.toggle('enabled');
-    });
+function toggleDarkTheme() {
+    // Toggle 'dark' and 'enabled' classes on all relevant elements
+    $('.post-blog, input[type="text"], textarea, header, footer, body, .blogs-web-button, .back-button, .dark-theme-toggle').toggleClass('dark enabled');
 
+    // Check if dark theme is currently enabled on the toggle button
+    const isDarkThemeEnabled = $('.dark-theme-toggle').hasClass('enabled');
+
+    // Store the state of the dark theme in local storage
+    localStorage.setItem('darkThemeEnabled', isDarkThemeEnabled);
+}
+
+$(document).ready(function() {
+    // Retrieve the value of darkThemeEnabled from local storage
+    const isDarkThemeEnabled = localStorage.getItem('darkThemeEnabled') === 'true';
+
+    // If darkThemeEnabled is true, apply the dark theme
+    if (isDarkThemeEnabled) {
+        toggleDarkTheme();
+    }
+});
+
+function sumbitBlogPost() {
+    
 }
 
 function navigateToBlogsPage() {
